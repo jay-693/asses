9f4012abf6bab19c8fe762a90c1ac3ba911abd0f
vijay <ghjn>
2024-05-27 14:20:10 +0530

    ls commit

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -21,6 +21,8 @@ END
 GO
 select dbo.get_numeric('24gsehjn34') as num;
 
+
+
 --Write a script to calculate age based on the Input DOB
 create table emp(empid int,empname nvarchar(100),dob datetime);
 insert into emp values(123,'a','2002-09-30');
@@ -47,11 +49,17 @@ GO
 drop function cal_age;
 select empid,empname,dbo.cal_age(dob) as age from emp;
 
+
+
 --Create a column in a table and that should throw an error when we do SELECT * or SELECT of that column. If we select other columns then we should see results
 Alter table emp add tot as (1/0);
 select empid,empname,dob from emp;
 select * from emp;
 select tot from emp;
+
+
+
+-- Display Calendar Table based on the input year
 CREATE TABLE Calendar
 (
     [Date] DATE PRIMARY KEY,
@@ -61,34 +69,78 @@ CREATE TABLE Calendar
     Month INT,
     DayOfMonth INT
 );
-CREATE PROCEDURE GenerateCalendarForYear
+CREATE PROCEDURE CalendarForYear
     @Year INT
 AS
 BEGIN
-    -- Clear any existing data for the specified year
-    DELETE FROM Calendar WHERE YEAR([Date]) = @Year;
-    
-    -- Declare variables
+    DELETE FROM Calendar;
     DECLARE @StartDate DATE = DATEFROMPARTS(@Year, 1, 1);
     DECLARE @EndDate DATE = DATEFROMPARTS(@Year, 12, 31);
     DECLARE @CurrentDate DATE = @StartDate;
-    
-    -- Loop through each day of the year
     WHILE @CurrentDate <= @EndDate
     BEGIN
-        -- Calculate the required attributes
         DECLARE @DayOfYear INT = DATEPART(DAYOFYEAR, @CurrentDate);
         DECLARE @Week INT = DATEPART(WEEK, @CurrentDate);
         DECLARE @DayOfWeek INT = DATEPART(WEEKDAY, @CurrentDate);
         DECLARE @Month INT = DATEPART(MONTH, @CurrentDate);
         DECLARE @DayOfMonth INT = DATEPART(DAY, @CurrentDate);
-        
-        -- Insert the data into the Calendar table
         INSERT INTO Calendar ([Date], DayOfYear, Week, DayOfWeek, Month, DayOfMonth)
         VALUES (@CurrentDate, @DayOfYear, @Week, @DayOfWeek, @Month, @DayOfMonth);
         
-        -- Move to the next day
         SET @CurrentDate = DATEADD(DAY, 1, @CurrentDate);
     END
 END
-EXEC GenerateCalendarForYear @Year = 2017;
+drop procedure CalendarForYear;
+EXEC CalendarForYear @Year = 2023;
+select * from Calendar;
+
+
+
+--Display Emp and Manager Hierarchies based on the input till the topmost hierarchy. (Input would be empid) Output: Empid, empname, managername, heirarchylevel
+create table Employee(empid INT PRIMARY KEY,empname VARCHAR(100),managerid INT);
+drop table Employee;
+insert into Employee VALUES(1,'John', 4);
+insert into Employee VALUES(2, 'Alice', 3);
+insert into Employee VALUES(3, 'Bob', 4);
+insert into Employee VALUES(4, 'Carol', 5);
+insert into Employee VALUES(5, 'David', NULL);
+select * from employee;
+
+CREATE PROCEDURE GetEmployeeHierarchy
+    @InputEmployeeId INT
+AS
+BEGIN
+    WITH EmployeeHierarchy AS (
+        SELECT 
+            empid, 
+            empname, 
+            CAST(NULL AS VARCHAR(100)) AS managername, 
+            0 AS hierarchy_level
+        FROM 
+            Employee
+        WHERE 
+            empid = @InputEmployeeId 
+
+        UNION ALL
+
+        SELECT 
+            e.empid, 
+            e.empname, 
+            CAST(m.empname AS VARCHAR(100)) AS managername,
+            eh.hierarchy_level + 1
+        FROM 
+            Employee e
+        INNER JOIN 
+            EmployeeHierarchy eh ON e.managerid = eh.empid
+        JOIN 
+            Employee m ON e.managerid = m.empid
+    )
+    SELECT 
+        empid, 
+        empname, 
+        managername, 
+        hierarchy_level
+    FROM 
+        EmployeeHierarchy;
+END;
+EXEC GetEmployeeHierarchy @InputEmployeeId = 4; 
